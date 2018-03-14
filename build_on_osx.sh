fcdbd1b6fc84aedb063c2b83edd8dfea0186b826
quran <qurandeyouxiang@qq.com>
2018-03-14 13:07:55 +0800

    modify script

--- build_on_osx.sh
+++ build_on_osx.sh
@@ -1,15 +1,19 @@
-brew update > /dev/null 
-brew install qt5 > /dev/null 
+brew update # > /dev/null 
+brew install qt5 #> /dev/null 
 export PATH=/usr/local/opt/qt5/bin:$PATH
 git clone --recursive https://github.com/uglide/RedisDesktopManager.git -b 0.9 rdm 
-cd ./rdm/src  
+cd ./rdm 
+if [ ! -d "./bin/osx/release" ]; then 
+    mkdir -p ./bin/osx/release 
+fi 
+cd ./src  
 ./configure
-cd ./3rdparty/crashreporter/
+cd ../3rdparty/crashreporter/
 sudo qmake CONFIG-=debug 
 sudo make 
-cp crashreporter ../../bin/osx/release ``
+cp crashreporter ../../bin/osx/release 
 cd ../../src/resources
-cp ./Info.plist.example Info.plist 
+cp ./Info.plist.sample Info.plist 
 cd ..
 qmake CONFIG-=debug 
 make -s -j4
